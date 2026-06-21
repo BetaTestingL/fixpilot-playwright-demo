5c552bf15ddd8823aa7899c4974ef141e3b4ebec
Anubhav Pathak <133781113+BetaTestingL@users.noreply.github.com>
2026-06-20 21:59:27 -0600

    Change homepage test to Google and update title check
    Updated test to load Google homepage and check title.

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,11 +1,5 @@
 const { test, expect } = require('@playwright/test');
 
-test('homepage loads', async ({ page }) => {
-  await page.goto('https://example.com');
-
-  await expect(page).toHaveTitle(/Example/);
-});
-
 test("homepage loads", async ({ page }) => {
   await page.goto("https://google.com");
 
